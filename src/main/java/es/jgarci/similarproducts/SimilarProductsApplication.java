72cd40ac7c51a040eed76e7f45f9eff85baa8fe1
Jose García <jose.garcia.escribano.bs@gmail.com>
2025-11-04 02:31:24 +0100

    initial base project

---
+++ src/main/java/es/jgarci/similarproducts/SimilarProductsApplication.java
@@ -0,0 +1,13 @@
+package es.jgarci.similarproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductsApplication.class, args);
+	}
+
+}
